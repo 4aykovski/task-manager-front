a4b8ad9c860a7a207a49be02640230035ea85aca
4aykovski <kan89kan@gmail.com>
2024-02-12 14:58:05 +0300

    fix: logo is image now

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import classes from './Logo.module.css'
 
 const Logo = () => {
+    const iconsDir = `${process.env.PUBLIC_URL}/assets/icons/`
+
     return (
-        <div className={classes.logo}/>
+        <img
+            src={`${iconsDir}logo.svg`}
+            className={classes.logo}
+            alt=""/>
     );
 };
 
