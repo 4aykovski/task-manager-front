b945d5a9328cc3fc82b64a8fb0f8a05b9570ca6c
4aykovski <kan89kan@gmail.com>
2024-03-04 15:05:35 +0700

    feat: project card

--- src/components/ProjectsCard/ProjectsCard.tsx
+++ src/components/ProjectsCard/ProjectsCard.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Card from "../ui/Card/Card";
 import classes from "./ProjectsCard.module.css"
+
 const ProjectsCard = () => {
     return (
         <Card className={classes.ProjectCard}>
+            <div className={classes.Heading}>
+                Project1
+            </div>
+            <div className={classes.ProjectBoard}>
+                <div className={classes.BoardsList}>
 
+                </div>
+                <div className={classes.TasksColumns}>
+
+                </div>
+            </div>
         </Card>
     );
 };
